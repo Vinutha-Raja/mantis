591e44abef6b19b06e9fa625b75c47872f4621eb
Vinutha Raja <vinutha8.raj@gmail.com>
2023-04-22 22:45:50 -0400

    Evaluation script init

---
+++ scripts/evaluation.py
@@ -0,0 +1,33 @@
+kmer_result_file = 'query_kmer.res'
+lmer_result_file = 'query.res'
+
+def read_result_file(filename, theta):
+    result_list = []
+    with open(filename, 'r') as f:
+        line = f.readline()
+        line.strip()
+        val = line.split('\t')
+        total_kmer = val[1]
+        while line:
+            line = f.readline()
+            if line:
+                line.strip()
+                val = line.split('\t')
+                file_name = val[0]
+                count = val[1]
+                ratio = int(count)/int(total_kmer)
+                if ratio > theta:
+                    result_list.append(file_name)
+    return result_list
+
+kmer_result = read_result_file(kmer_result_file, 0.0)
+print(kmer_result)
+lmer_result = read_result_file(lmer_result_file, 0.0)
+print(lmer_result)
+
+
+
+
+
+
+
